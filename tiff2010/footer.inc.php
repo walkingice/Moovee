77c3d27b1dc9a69528428f264e5d1a7e495a499b
CornGuo <cornguo@msn.com>
2014-10-16 14:08:20 +0800

    footer link to about.me

--- tiff2010/footer.inc.php
+++ tiff2010/footer.inc.php
@@ -2,7 +2,7 @@
     <div class="fb_like span1"><object data="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fcornguo.atcity.org%2Ftest%2Fmoovee%2F&amp;layout=button_count&amp;show_faces=true&amp;width=100&amp;action=like&amp;colorscheme=light&amp;height=21" style="border:none; overflow:hidden; height:21px;" type="text/html"></object></div>
     <div class="span11">
         節目資訊以節目手冊與台北電影節佈告為準，本頁面僅供輔助參考<br />
-        <a href="http://cornguo.atcity.org">CornGuo</a> @ 20100519<a href="../changelog.txt" title="changelog">,</a> 官網資料很難 parse，最後還是一行一行的敲進電腦了.. Orz
+        <a href="http://about.me/cornguo">CornGuo</a> @ 20100519<a href="../changelog.txt" title="changelog">,</a> 官網資料很難 parse，最後還是一行一行的敲進電腦了.. Orz
         (<?php echo "# {$counter}"; ?>)
     </div>
 </div>
