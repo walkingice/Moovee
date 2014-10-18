d74d1e2dfe1854fbe9a7bc20ecbd8bd4df9ae2fb
Julian_Chu <walkingice@0xlab.org>
2014-10-18 16:19:10 +0800

    ng: start implementing angularjs app
    when launched, it will load movie json file and display
    movies list.

---
+++ ng_gh2014/app.js
@@ -0,0 +1,26 @@
+(function(window, angular) {
+    var app = angular.module('mooveeApp', []);
+
+    app.run(['$rootScope', '$http', function ($rs, $http) {
+        var promise = $http.get('data/gh2014.json');
+        $rs.loading = true;
+        promise.success(function (data, status) {
+            $rs.loading = false;
+            $rs.movieData = data;
+            debugger;
+        });
+
+        promise.error(function (data, status) {
+            $rs.loading = false;
+        });
+    }]);
+
+    app.controller('ItemsCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
+        $scope.$watch('movieData', function (nV) {
+            if (!nV) {
+                return;
+            }
+            $scope.items = nV.items;
+        });
+    }]);
+})(window, angular);
--- ng_gh2014/index.inc.php
+++ ng_gh2014/index.inc.php
@@ -7,7 +7,7 @@
 <?php include("../includes/header.test.inc.php"); ?>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
 </head>
-<body onload="render();">
+<body ng-app="mooveeApp">
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="navbar-inner">
         <div class="container-fluid">
@@ -20,22 +20,29 @@
 <div class="container-fluid">
     <div id="breadcrumb" class="well well-small dropdown">
         <div class="btn-group">
-            <span class="btn btn-primary">點此開始</span><span id="bCat-dropdown" class="dropdown-toggle btn btn-primary" data-toggle="dropdown"><b class="caret"></b></span>
+            <span class="btn btn-primary">排序方法</span><span id="bCat-dropdown" class="dropdown-toggle btn btn-primary" data-toggle="dropdown"><b class="caret"></b></span>
             <ul id="tabs" class="dropdown-menu" role="menu" aria-labelledby="bCat-dropdown">
-                <li role="menuitem"><a id="by-CATEGORY" class="tab" onclick="getGroup(this);return false;" href="#CATEGORY">影展分類</a></li>
-                <li role="menuitem"><a id="by-DATE" class="tab" onclick="getGroup(this);return false;" href="#DATE">播映日期</a></li>
-                <li role="menuitem"><a id="by-PLACE" class="tab" onclick="getGroup(this);return false;" href="#PLACE">播映影廳</a></li>
-                <li role="menuitem"><a id="by-GRADE" class="tab" onclick="getGroup(this);return false;" href="#GRADE">電影分級</a></li>
-                <li role="menuitem"><a id="by-REMARK" class="tab" onclick="getGroup(this);return false;" href="#REMARK">場次備註</a></li>
+                <li role="menuitem"><a class="tab" href="#CATEGORY">影展分類</a></li>
+                <li role="menuitem"><a class="tab" href="#DATE">播映日期</a></li>
+                <li role="menuitem"><a class="tab" href="#PLACE">播映影廳</a></li>
+                <li role="menuitem"><a class="tab" href="#GRADE">電影分級</a></li>
+                <li role="menuitem"><a class="tab" href="#REMARK">場次備註</a></li>
             </ul>
         </div>
     </div>
 </div>
 
-<div class="container-fluid">
+<div class="container-fluid" ng-controller="ItemsCtrl">
     <div class="row-fluid">
-        <div id="lPanel" class="span5 well well-small">
-            <div id="dragBox"></div>
+        <div id="lPanel" class="span5 well">
+            <table class="table">
+                <tr ng-repeat="item in items">
+                    <td>{{item.CTITLE}}</td>
+                    <td>{{item.PLACE}}</td>
+                    <td>{{item.REMARK}}</td>
+                    <td>{{item.GRADE}}</td>
+                </tr>
+            </table>
             <div id="remarkDesc"><?php echo $remarkDesc;?></div>
         </div>
         <div id="dropBox" class="span7 well well-small"></div>
@@ -47,4 +54,5 @@
 <div id="varStor" class="hidden"><?php if(strlen($movs) > 0) echo $movs;?></div>
 <div id="filter" class="hidden"></div>
 </body>
+<script src="app.js"></script>
 </html>
